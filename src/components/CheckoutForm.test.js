9f425297bb535aaa7fdb596fb051743f79ecb981
rstott87 <rstott87@yahoo.com>
2022-01-14 13:19:13 -0500

    all tests are passing

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,7 +1,9 @@
 import React from "react";
+import '@testing-library/jest-dom/extend-expect';
 import MutationObserver from 'mutationobserver-shim';
-import { render, waitFor } from "@testing-library/react";
+import { getByRole, render, screen, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
@@ -12,11 +14,12 @@ test("renders without errors", () => {
 
 test("shows success message on submit with form details", async () => {
     render(<CheckoutForm/>);
+    const submitButton = screen.getByRole("button");
+    userEvent.click(submitButton);
 
     await waitFor(()=>{
-        const successMessage = screen.queryByTestId("succesMessage");
+        const successMessage = screen.queryByText(/You have ordered some plants!/i);
         expect(successMessage).toBeInTheDocument();
-    });
-    
-
+    });    
 });
+
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -3,21 +3,28 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-  state = {
+  
+  constructor(){
+    super();
+  this.state = {
     plants:[]
   }
+}
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
   componentDidMount() {  
   axios.get('http://localhost:3333/plants')
       .then(resp => {
-        this.state.plants = resp.data
+        this.setState ({
+          ...this.state,
+          plants: resp.data
+        });
       })
       .catch(error => {
         console.log(error);
-      });
-  };
+        });
+      } 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
